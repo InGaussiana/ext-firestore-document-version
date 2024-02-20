cf8ae8d8d8f131cffd7e321448b1c0e613ccff3b
EAR <sinedie@protonmail.com>
2024-02-20 18:20:58 -0500

    add extension config on yaml and change default field names

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -3,9 +3,9 @@ export const config = {
   enableRestore: process.env.ENABLE_RESTORE ?? true,
   historyPath: `${process.env.HISTORY_COLLECTION ?? "history"}/database`,
   ignoreFields: process.env.IGNORE_FIELDS ?? "",
-  restoreField: process.env.RESTORE_FIELD ?? "restore",
-  gotoField: process.env.GOTO_FIELD ?? "goto",
-  undoField: process.env.UNDO_FIELD ?? "undo",
-  redoField: process.env.REDO_FIELD ?? "redo",
+  restoreField: process.env.RESTORE_FIELD ?? "_restore",
+  gotoField: process.env.GOTO_FIELD ?? "_goto",
+  undoField: process.env.UNDO_FIELD ?? "_undo",
+  redoField: process.env.REDO_FIELD ?? "_redo",
   versionField: process.env.VERSION_FIELD ?? "_version",
 };
